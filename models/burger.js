efcab89fe39adab799654f3da4aa4b40210354b6
Shane Lennon <shane.michael.lennon@gmail.com>
2019-04-01 18:04:59 -0500

    Almost working

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,21 @@
-// Import the ORM to insertOne functions that will interact with the database.
-var orm = require("../config/orm.js");
+var orm = require ("../config/orm.js");
 
 var burger = {
-  selectAll: function(callback) {
-    orm.selectAll("burgers", function(response) {
-      callback(response);
-    });
-  },
-  // The variables cols and vals are arrays.
-  insertOne: function(cols, vals, callback) {
-    orm.insertOne("burgers", cols, vals, function(response) {
-      callback(response);
-    });
-  },
-  updateOne: function(objColVals, condition, callback) {
-    orm.updateOne("burgers", objColVals, condition, function(response) {
-      callback(response);
-    });
-  }
+    all: function(cb) {
+    	orm.all("burgers", function(res){
+    		cb(res);
+    	});
+    },
+    create: function(cols, vals, cb){
+    	orm.create("burgers", cols, vals, function(res){
+    		cb(res);
+    	});
+    },
+    update: function(objColVals, condition, cb){
+    	orm.update("burgers", objColVals, condition, function(res){
+    		cb(res);
+    	});
+    }
 };
 
-// Export the database functions for the controller (burgersController.js).
 module.exports = burger;
